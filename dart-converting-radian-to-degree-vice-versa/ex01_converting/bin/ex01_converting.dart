9591abccae91ae8445dad026ff746c161c435a52
Anatolii Kosorukov <java1cprog@yandex.ru>
2020-07-22 13:50:02 +0200

    Converting Radian To Degree & Vice Versa

--- dart-converting-radian-to-degree-vice-versa/ex01_converting/bin/ex01_converting.dart
+++ dart-converting-radian-to-degree-vice-versa/ex01_converting/bin/ex01_converting.dart
@@ -3,10 +3,10 @@
 import 'package:vector_math/vector_math.dart';
 
 void main() {
-    degrees(0); // 0.0
-	  degrees(1); // 57.29577951308232
-    radians(0); // 0.0
-	  radians(57.29577951308232); // 1.0
-	  radians(90); // 1.5707963267948966
-	  radians(360); // 6.283185307179586xx
+    print(degrees(0)); // 0.0
+	  print(degrees(1)); // 57.29577951308232
+    print(radians(0)); // 0.0
+	  print(radians(57.29577951308232)); // 1.0
+	  print(radians(90)); // 1.5707963267948966
+	  print(radians(360)); // 6.283185307179586xx
 }
